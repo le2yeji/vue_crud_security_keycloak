2f8a69ecbdf9bd9714c08de5ac20247ab9817af4
le2yeji <96720279+le2yeji@users.noreply.github.com>
2024-02-21 15:11:13 +0900

    Update fetch-wrapper.js

--- src/helpers/fetch-wrapper.js
+++ src/helpers/fetch-wrapper.js
@@ -32,12 +32,11 @@ function authHeader() {
 
   // return auth header with jwt if user is logged in and request is to the api url
   const { user } = JSON.parse(localStorage.getItem('auth'));
-  const isLoggedIn = JSON.parse(localStorage.getItem('isLoggedIn'));
+  const token = user.accessToken; // JSON.parse(user.accessToken);
+  console.log('token', token);
 
-  console.log('isLoggedIn', isLoggedIn);
-
-  if (isLoggedIn) {
-    return { Authorization: `Bearer ${user.accessToken}` };
+  if (token) {
+    return { Authorization: `Bearer ${token}` };
   } else {
     return {};
   }

3597cacb57c76972e7ab438c267be2b0fe137db1
le2yeji <leyagi17@gmail.com>
2024-02-28 17:50:52 +0900

    login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -17,6 +17,35 @@ const onSubmit = async () => {
           <div class="text-h6 text-center">로그인</div>
         </q-card-section>
         <q-card-section class="q-mb-none q-gutter-y-lg">
+            <q-input
+              filled
+              type="text"
+              v-model="username"
+              label="ID *"
+              hint="for test: test"
+              lazy-rules
+              :rules="[(val) => (val && val.length > 0) || 'Please type username(ID)']"
+            />
+            <q-input
+              filled
+              type="password"
+              v-model="password"
+              label="Password *"
+              hint="for test: test"
+              lazy-rules
+              :rules="[(val) => (val !== null && val !== '') || 'Please type your password']"
+            />
+            <q-btn
+              type="submit"
+              unelevated
+              color="primary"
+              size="lg"
+              class="full-width"
+              label="Login"
+              :disable="!username || !password"
+            />
+        </q-card-section>
+        <q-card-section class="q-mb-none q-gutter-y-lg">
           <q-btn
             type="submit"
             unelevated

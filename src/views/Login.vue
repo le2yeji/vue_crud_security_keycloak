9fd492c6b4550f55c46859adde84c8a5d019fbc7
le2yeji <leyagi17@gmail.com>
2024-03-05 08:49:58 +0900

    c

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -98,8 +98,9 @@ const encryptLoginData = async () => {
   form.value.encryptIdPassword = encryptedData;
   form.value.username = ''; // ref 변수에 접근
   form.value.password = ''; // ref 변수에 접근
-
-  const user = await fetchWrapper.post(`${baseUrl}/init-rsa`, form.value.encryptIdPassword);
+  
+  // const user = await fetchWrapper.post(`${baseUrl}/init-rsa`, form.value.encryptIdPassword);
+  const user = await fetchWrapper.post(`${baseUrl}/init-rsa`, { id, pw });
   console.log('user', user);
 
   // store user details and jwt in local storage to keep user logged in between page refreshes
